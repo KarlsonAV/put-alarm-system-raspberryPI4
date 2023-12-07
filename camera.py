15fd660526f8a7b1fe478257dba6f8a2eea0ea53
Andrei Kartavik <andreykartavik@gmail.com>
2023-12-07 21:07:22 +0100

    [Fix] take_photo returns string

--- camera.py
+++ camera.py
@@ -5,4 +5,12 @@ class Camera:
 
     @staticmethod
     def take_photo(output_path: str) -> str:
-        os.system(f"libcamera-still -o {output_path}")
+        output = ""
+        try:
+            os.system(f"libcamera-still -o {output_path}")
+            output = f"Successfully saved photo {output_path}"
+        except Exception as e:
+            output = f"Error: {e}\nCouldn't save photo {output_path}"
+            
+        return output
+            
